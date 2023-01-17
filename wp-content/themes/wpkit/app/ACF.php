daf1d3332cf9d81e54e6a46dfd3d013cd6594811
Garegin Hakobyan <garik.28h@gmail.com>
2023-01-17 17:17:47 +0100

    Create ACF Class

---
+++ wp-content/themes/wpkit/app/ACF.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App;
+
+class ACF {
+  
+  public function __construct() {
+    add_action( 'acf/init', [ $this, 'acf_register_options_page' ] );
+  }
+  
+  /**
+   * ACF Register Options Page.
+   *
+   * @return array|false
+   */
+  public function acf_register_options_page(): bool|array {
+    if ( ! function_exists( 'acf_add_options_page' ) ) {
+      return false;
+    }
+    
+    $option_pages = [
+      [
+        'page_title' => 'General Settings',
+        'menu_title' => 'Theme Settings',
+        'menu_slug'  => 'general-settings',
+        'capability' => 'edit_posts',
+        'redirect'   => true,
+        'sub_page'   => false,
+      ],
+      [
+        'page_title'  => 'Footer Settings',
+        'menu_title'  => 'Footer',
+        'parent_slug' => 'general-settings',
+        'sub_page'    => true,
+      ],
+    ];
+    
+    foreach ( $option_pages as $option ) {
+      if ( ! $option['sub_page'] ) {
+        $option_page = acf_add_options_page( $option );
+      } else {
+        $option_page = acf_add_options_sub_page( $option );
+      }
+    }
+    
+    return $option_page;
+  }
+}
+
+new ACF();
