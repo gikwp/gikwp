f59059d07370caf1cef63f84da98686e6f1e27da
Garegin Hakobyan <garik.28h@gmail.com>
2023-01-19 13:28:26 +0100

    Update section about-info template

--- wp-content/themes/wpkit/resources/views/partials/about/about-info.php
+++ wp-content/themes/wpkit/resources/views/partials/about/about-info.php
@@ -14,14 +14,16 @@ $me_image = get_field( 'image_im' );
 ?>
 <section class="about-info">
     <div class="row">
-        <div class="col-md-2">
-            <figure class="image-block mb-0">
-				<?php
-				$image_size = apply_filters( 'wpkit_one_attachment_size', 'medium' );
-				echo wp_get_attachment_image( $me_image, $image_size, false, [ 'class' => 'rounded-4 img-fluid' ] );
-				?>
-            </figure>
-        </div>
+		<?php if ( ! empty( $me_image ) ): ?>
+            <div class="col-md-2">
+                <figure class="image-block mb-0">
+					<?php
+					$image_size = apply_filters( 'wpkit_one_attachment_size', 'medium' );
+					echo wp_get_attachment_image( $me_image, $image_size, false, [ 'class' => 'rounded-4 img-fluid' ] );
+					?>
+                </figure>
+            </div>
+		<?php endif; ?>
         <div class="col-md-8">
             <h1 class="display-3"><?php the_title(); ?></h1>
 			<?php echo $me_description; ?>
