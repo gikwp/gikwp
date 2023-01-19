caccfa58e506c5b767961d3d4696bd9f1a5b42b8
Garegin Hakobyan <garik.28h@gmail.com>
2023-01-19 11:09:09 +0100

    Remove contact form html <span> and <p> tags.

--- wp-content/themes/wpkit/app/filters.php
+++ wp-content/themes/wpkit/app/filters.php
@@ -11,6 +11,17 @@ namespace App;
  *
  * @return string
  */
-add_filter('excerpt_more', function () {
-    return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Read more', 'wpkit'));
-});
+add_filter( 'excerpt_more', function () {
+	return sprintf( ' &hellip; <a href="%s">%s</a>', get_permalink(), __( 'Read more', 'wpkit' ) );
+} );
+
+/**
+ * Replace contact form html <span> and <p> tags.
+ */
+add_filter( 'wpcf7_form_elements', function ( $content ) {
+	$content = preg_replace('/<(span).*?class="\s*(?:.*\s)?wpcf7-form-control-wrap(?:\s[^"]+)?\s*"[^\>]*>(.*)<\/\1>/i', '\2', $content);
+	
+	return $content;
+} );
+
+add_filter('wpcf7_autop_or_not', '__return_false');
