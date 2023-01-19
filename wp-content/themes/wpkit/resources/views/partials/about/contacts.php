f1705c0d4e6d646dbe401d783a481adbaa9d0233
Garegin Hakobyan <garik.28h@gmail.com>
2023-01-19 13:23:13 +0100

    Update section contacts template

--- wp-content/themes/wpkit/resources/views/partials/about/contacts.php
+++ wp-content/themes/wpkit/resources/views/partials/about/contacts.php
@@ -1,54 +1,71 @@
 <?php
 
 /**
- * Displays section Contacts.
+ * Displays section contacts.
  */
+
+$get_forms   = get_field( 'contact_forms' );
+$description = get_field( 'contact_description' );
+$heading     = get_field( 'contact_heading' );
+$get_links   = get_field( 'contact_links' );
+
+$title = $get_forms['title'];
+$forms = $get_forms['forms'];
+$links = $get_links['contacts'];
+$label = $get_links['label'];
+
+$col_forms_class = $description ? 'col-7' : 'col-12';
+$col_info_class  = $forms ? 'col-5' : 'col-12';
 
 ?>
 <div class="separated my-lg-5"></div>
 <section id="contact" class="contact-me">
-    <h3 class="display-3">
-        Get in touch.
-    </h3>
+	<?php if ( ! empty( $heading ) ): ?>
+        <h3 class="display-3">
+			<?php echo esc_html( $heading ); ?>
+        </h3>
+	<?php endif; ?>
     <div class="row">
-        <div class="col-5">
-            <div class="contact-info d-flex flex-column bg-cinder p-4 h-100">
-                <h5 class="mb-3">What’s your story? Get in touch</h5>
-                <p>
-                    Hello! I’m available for freelance work. If you have something that you need help with, feel free to
-                    contact me at any time.
-                </p>
-                <p>
-                    I believe that all interaction between our customers and me should be carried out
-                    <strong>only by email</strong>. I don’t want to lose or smudge over telephone conversations. Short or
-                    insignificant
-                    moments can be solved through instant messengers or social networks.
-                </p>
-
-                <div class="contact-links mt-auto">
-                    <label class="text-white fw-light mb-2">Contact Me:</label>
-                    <ul class="list-unstyled mb-0">
-                        <li>
-                            <i class="bi bi-envelope-fill text-emerald"></i>
-                            <a href="mailto:support@gikwp.com" target="_blank">
-                                Send me an email
-                            </a>
-                        </li>
-                        <li>
-                            <i class="bi bi-skype text-emerald"></i>
-                            <a href="https://join.skype.com/invite/CAyTiWJ1Nd5q" target="_blank">
-                                Send me a message
-                            </a>
-                        </li>
-                    </ul>
+		<?php if ( ! empty( $description ) ): ?>
+            <div class="<?php echo esc_html( $col_info_class ); ?>">
+                <div class="contact-info d-flex flex-column bg-cinder p-4 h-100">
+                    <div class="contact-contains">
+						<?php echo $description; ?>
+                    </div>
+					<?php if ( $links ): ?>
+                        <div class="contact-links mt-auto">
+							<?php if ( $label ): ?>
+                                <label class="text-white fw-light mb-2">
+									<?php echo esc_html( $label ); ?>
+                                </label>
+							<?php endif; ?>
+                            <ul class="list-unstyled mb-0">
+								<?php foreach ( (array) $links as $link ): ?>
+                                    <li>
+                                        <i class="bi bi-<?php echo esc_attr( $link['icon'] ); ?> text-emerald"></i>
+                                        <a href="<?php echo esc_url( $link['link'] ); ?>"
+											<?php echo $link['target'] ? esc_attr( 'target="_blank"' ) : ''; ?>
+                                        >
+											<?php echo esc_html( $link['text'] ); ?>
+                                        </a>
+                                    </li>
+								<?php endforeach; ?>
+                            </ul>
+                        </div>
+					<?php endif; ?>
                 </div>
             </div>
-        </div>
-        <div class="col-7">
-            <div class="contact-form bg-cinder p-4 h-100">
-                <h5 class="mb-3">Say Something</h5>
-				<?php echo do_shortcode( '[contact-form-7 id="5" title="Contact form"]' ); ?>
+		<?php endif; ?>
+		
+		<?php if ( $forms ): ?>
+            <div class="<?php echo esc_attr( $col_forms_class ); ?>">
+                <div class="contact-form bg-cinder p-4 h-100">
+					<?php if ( $title ): ?>
+                        <h5 class="mb-3"><?php echo esc_html( $title ); ?></h5>
+					<?php endif; ?>
+					<?php echo do_shortcode( $forms ); ?>
+                </div>
             </div>
-        </div>
+		<?php endif; ?>
     </div>
 </section>
